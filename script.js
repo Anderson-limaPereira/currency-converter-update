ff8baa9380caf03b12e3ae97fd2dd6618be87e56
@Anderson-limaPereira <161944582+Anderson-limaPereira@users.noreply.github.com>
2024-11-05 21:12:16 -0300

    Adicionando valores real no codigo

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+
 const button = document.querySelector("button")
 const select = document.querySelector(".valor-select")
 
-function convertervalor() {
+const convertervalor = async () => {
 
   const valorinputvalue = document.querySelector(".valor-input").value
 
   const valorDaMoedaConverter = document.querySelector(".valor-da-moeda-converter")
   const valorDaMoedaConvertido = document.querySelector(".valor-da-moeda-convertido")
 
-
-
-  const dolartoday = 5.2
-  const eurotoday = 6.2
+  const data = await fetch(" https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,BTC-BRL").then(res => res.json())
   
-  const libratoday = 6.7
-  const bitcointoday = 367.582
- 
+  const dolar = data.USDBRL.high
+  const euro = data.EURBRL.high
+  const libra = data.GBPBRL.high
+  const bitcoin = data.BTCBRL.high
+   
+
 
   if (select.value == "dolar") {
     valorDaMoedaConvertido.innerHTML = new Intl.NumberFormat("en-US", {
@@ -23,7 +24,7 @@ function convertervalor() {
       style: "currency",
       currency: "USD"
 
-    }).format(valorinputvalue / dolartoday)
+    }).format(valorinputvalue / dolar)
 
   }
 
@@ -33,7 +34,7 @@ function convertervalor() {
       style: "currency",
       currency: "EUR"
 
-    }).format(valorinputvalue / eurotoday)
+    }).format(valorinputvalue / euro)
 
   }
   
@@ -43,7 +44,7 @@ function convertervalor() {
       style: "currency",
       currency: "GBP"
 
-    }).format(valorinputvalue / libratoday)
+    }).format(valorinputvalue / libra)
 
   }
 
@@ -53,10 +54,10 @@ function convertervalor() {
       style: "currency",
       currency: "BTC"
 
-    }).format(valorinputvalue / bitcointoday)
+    }).format(valorinputvalue / bitcoin)
 
   }
-    
+
 
   valorDaMoedaConverter.innerHTML = new Intl.NumberFormat("pt-BR", {
 
@@ -66,43 +67,45 @@ function convertervalor() {
   }).format(valorinputvalue)
 
 }
-  function changecurrency() {
-    const valorNome = document.getElementById("valor-nome")
-    const valorImg = document.querySelector(".valor-img")
+
+
+function changecurrency() {
+  const valorNome = document.getElementById("valor-nome")
+  const valorImg = document.querySelector(".valor-img")
 
 
 
-    if (select.value == "dolar") {
+  if (select.value == "dolar") {
 
-      valorNome.innerHTML = "Dólar Americano"
-      valorImg.src = "./img/usa.png"
-    }
-
-    if (select.value == "euro") {
-
-      valorNome.innerHTML = "euro"
-      valorImg.src = "./img/euro.png"
-    }
-    
-    if (select.value == "libra") {
-
-      valorNome.innerHTML = "libra"
-      valorImg.src = "./img/libra.png"
-    }
-
-    if (select.value == "bitcoin") {
-
-      valorNome.innerHTML = "bitcoin"
-      valorImg.src = "./img/bitcoin.png"
-    }
-     
-
-    
-    convertervalor()
-    
+    valorNome.innerHTML = "Dólar Americano"
+    valorImg.src = "./img/usa.png"
   }
 
+  if (select.value == "euro") {
+
+    valorNome.innerHTML = "euro"
+    valorImg.src = "./img/euro.png"
+  }
+  
+  if (select.value == "libra") {
+
+    valorNome.innerHTML = "libra"
+    valorImg.src = "./img/libra.png"
+  }
+  
+  if (select.value == "bitcoin") {
+    
+    valorNome.innerHTML = "bitcoin"
+    valorImg.src = "./img/bitcoin.png"
+  }
+  
+  convertervalor()
+}
 
 select.addEventListener("change", changecurrency)
 button.addEventListener("click", convertervalor)
+
+
+
+
 
